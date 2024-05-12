3dbea08535a8c885d431c2a24381f5cd13ecb385
Thomas Stubblefield <95105859+SerenityUX@users.noreply.github.com>
2024-05-11 22:34:11 -0400

    RTMP Docker Fixes

---
+++ rtmp/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Requesting Let's Encrypt certificate
+certbot --nginx -d serenidad.click -d www.serenidad.click --non-interactive --agree-tos --email your-email@example.com --redirect
+
+# Setting up automatic renewal in a cron job
+echo "0 12 * * * /usr/bin/certbot renew --quiet" >> /etc/crontab
+
+# Start nginx in the foreground
+exec "$@"
