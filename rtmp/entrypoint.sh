e8304c8759f45bd1c9ad1c990df4b0674ee9b64a
Thomas Stubblefield <95105859+SerenityUX@users.noreply.github.com>
2024-05-11 22:41:22 -0400

    current

--- rtmp/entrypoint.sh
+++ rtmp/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Requesting Let's Encrypt certificate
-certbot --nginx -d serenidad.click -d www.serenidad.click --non-interactive --agree-tos --email your-email@example.com --redirect
+certbot --nginx -d serenidad.click -d www.serenidad.click --non-interactive --agree-tos --email thomas@serenidad.app --redirect
 
 # Setting up automatic renewal in a cron job
 echo "0 12 * * * /usr/bin/certbot renew --quiet" >> /etc/crontab
